be1a254104a5a5bed8cb4eaa95d3e789c4cdb431
NoScapeDev <105945687+NoScapeDev@users.noreply.github.com>
2023-08-22 03:12:18 +0100

    Add files via upload

--- src/main/java/net/devscape/project/minerave_ranks/managers/PlayerDataClass.java
+++ src/main/java/net/devscape/project/minerave_ranks/managers/PlayerDataClass.java
@@ -3,7 +3,6 @@ package net.devscape.project.minerave_ranks.managers;
 import lombok.Getter;
 import net.devscape.project.minerave_ranks.MineraveRanks;
 import net.devscape.project.minerave_ranks.handlers.Rank;
-import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
 @Getter
@@ -15,8 +14,8 @@ public class PlayerDataClass {
         return MineraveRanks.getMineraveRanks().getRankManager().getRank(MineraveRanks.getMineraveRanks().getMariadb().getRank(offlinePlayer.getUniqueId()));
     }
 
-    public String getRankName(OfflinePlayer offlinePlayer) {
-        return MineraveRanks.getMineraveRanks().getMariadb().getRank(offlinePlayer.getUniqueId());
+    public String getRankName(Player player) {
+        return MineraveRanks.getMineraveRanks().getMariadb().getRank(player.getUniqueId());
     }
 
     public boolean isAdmin(Player player) {
